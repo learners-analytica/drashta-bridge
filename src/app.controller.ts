a14941f82aa64eda9ea5dfc0fed931aa1147e74f
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-01-29 17:30:40 +0530

    update inital endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getServiceStatus() {
+    return this.appService.getServiceStatus();
   }
 }
+
