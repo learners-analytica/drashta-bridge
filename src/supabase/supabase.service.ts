2f1cce616db3eeacf357aeb3b55f1ae1f35b0b75
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-10 16:36:07 +0530

    update supabase driver

--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import type {SupabaseClient} from '@supabase/supabase-js';
 import { createClient } from '@supabase/supabase-js';
-import type {TTableStructure} from '@learners-analytica/drashta-types-ts';
+import type {TDataSeries, TTableStructure} from '@learners-analytica/drashta-types-ts';
 import * as dotenv from 'dotenv';
 @Injectable()
 export class SupabaseService {
@@ -9,15 +9,14 @@ export class SupabaseService {
 
   constructor() {
     // Replace these values with your Supabase project URL and public API key
-    const SUPABASE_URL = process.env.SUPABASE_URL || '';
-    const SUPABASE_KEY = process.env.SUPABASE_KEY || '';
+    const SUPABASE_URL = process.env.DATABASE_URL || '';
+    const SUPABASE_KEY = process.env.DATABASE_KEY || '';
     
     this.supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
   }
 
   async checkIfTableExists(tableName: string): Promise<boolean> {
-    const Tables:TTableStructure[] = await this.getTableStructData();
-    const tableNames: string[] = Tables.map((table) => table.table_name);
+    const Tables: TTableStructure[] = await this.getTableStructData();
     return Tables.some((table) => table.table_name === tableName);
   }
 
@@ -31,6 +30,24 @@ export class SupabaseService {
       throw error;
     }
 
+    return data;
+  }
+
+  async getColumnNameFromTable(table:string):Promise<string[]>{
+    const TableData = await this.getTableStructData()
+    const ColumnsData = TableData.filter((tableData)=> tableData.table_name === table)
+    const Columns = ColumnsData.map((columnsData)=> columnsData.table_columns)
+    return Columns
+  }
+  async getTableData(table: string, size: number = 1000, columns: string[] = null): Promise<any> {
+    const columnStr = columns ? columns.join(',') : '*';
+    const { data, error } = await this.supabase
+      .from(table)
+      .select(columnStr)
+      .limit(size)
+    if (error) {
+      throw error;
+    }
     return data;
   }
 
