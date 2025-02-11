a860ffc96b105e46a207b4d94b648c0c119f1a81
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-11 20:56:43 +0530

    add table data endpoint

--- src/supabase/supabase.controller.ts
+++ src/supabase/supabase.controller.ts
@@ -6,13 +6,18 @@ export class SupabaseController {
   constructor(private readonly supabaseService: SupabaseService) {}
 
   @Get('get-struct-supabase')
-  async getTableData() {
+  async getTableStructData() {
     return this.supabaseService.getTableStructData();
   }
 
   @Get('get-test-table-info')
   async getTableInfo() {
     return this.supabaseService.getColumnNameFromTable('contacts');
+  }
+
+  @Get('get-test-table-data')
+  async getTableData() {
+    return this.supabaseService.getTableData('contacts');
   }
 }
 
