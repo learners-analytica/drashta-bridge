9d6ee3038471be261d5d376cf3551c3e0c942e4e
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-16 09:59:41 +0530

    add CORS

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   const port = process.env.PORT ?? 3000;
   await app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
